f8167b13a949c0e2c259b79e3864b0151b6c9559
Ray Chng <10988b@gmail.com>
2019-07-18 17:54:33 +0800

    Add UI Basic UI elements
    :)))))))))))))))

---
+++ MDGE-DeepSpace/Assets/Imports/Scripts/Audio_Manager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Audio_Manager : MonoBehaviour
+{//this script is for controlling volumme
+    public AudioMixer audioMixer;
+  
+
+    public void SetVolumeMusic(float volume)
+    {
+        audioMixer.SetFloat("Music", volume);
+    }
+
+    public void SetVolumeSFX(float volume)
+    {
+        audioMixer.SetFloat("SFX", volume);
+    }
+
+    
+}
