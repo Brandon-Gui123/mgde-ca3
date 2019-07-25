168e14559131cafe3bf080b41c053b5906beda0f
Brandon_Gui123 <BrandonGui123Alt@gmail.com>
2019-07-25 17:59:25 +0800

    WIP: Camera follow script.

---
+++ MDGE-DeepSpace/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    [SerializeField]
+    private Camera mainCamera;
+
+    private Vector2 viewportSize;
+
+    // Awake is called when the script instance is being loaded
+    private void Awake() {
+        if (!mainCamera) {
+            mainCamera = Camera.main;
+        }        
+    }
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+}
