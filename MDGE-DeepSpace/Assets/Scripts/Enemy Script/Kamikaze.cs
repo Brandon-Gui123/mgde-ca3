1b5b350f96324d9267182d368cde774f42fc0285
GriefGod <51537376+GriefGod@users.noreply.github.com>
2019-07-26 14:47:46 +0800

    AI Test (#1)
    * Enemy spawner now based on map size.
    
    The enemy spawner is now based on the map's size, instead of based on the camera's viewport size.
    
    * WIP: Camera follow script.
    
    * Added camera following functionality.
    
    With documentation as well.
    
    * Add AI

--- MDGE-DeepSpace/Assets/Scripts/Enemy Script/Kamikaze.cs
+++ MDGE-DeepSpace/Assets/Scripts/Enemy Script/Kamikaze.cs
@@ -9,32 +9,44 @@ public class Kamikaze : MonoBehaviour
     public GameObject player;
 
     private Player_Health_Script playerhealth;
+    //AI
+    public float speed;
+    public Transform target;
+    public float stoppingDistance;
 
-   // private BoxCollider2D collider;
+
+    // private BoxCollider2D collider;
 
 
     // Start is called before the first frame update
     void Start()
     {
         playerhealth = player.GetComponent<Player_Health_Script>();
+        //AI
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //AI
+        if (Vector2.Distance(transform.position, target.position) > stoppingDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
 
     }
 
     void OnTriggerEnter2D(Collider2D collider)
-    {/*
+    {
         print("Player has been Kamikazed!");
-        if (collider.gameObject.CompareTag( "Player"))
+        if (collider.gameObject.CompareTag("Player"))
         {
 
             playerhealth.playerDamaged(damagedealt);
-          //  Destroy(this.gameObject);
+            Destroy(this.gameObject);
         }
-        */
+        
     }
 }
