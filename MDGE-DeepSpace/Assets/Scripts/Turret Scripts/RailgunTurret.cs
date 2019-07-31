cb5bf91a6c9ff2685c5adc4883426089637da0a8
Brandon_Gui123 <BrandonGui123Alt@gmail.com>
2019-07-31 09:26:49 +0800

    Added states to the railgun turret.

--- MDGE-DeepSpace/Assets/Scripts/Turret Scripts/RailgunTurret.cs
+++ MDGE-DeepSpace/Assets/Scripts/Turret Scripts/RailgunTurret.cs
@@ -6,6 +6,27 @@ public class RailgunTurret : MonoBehaviour
 {
 
     /// <summary>
+    /// An enumeration containing the possible states of this turret.
+    /// </summary>
+    private enum TurretState
+    {
+        /// <summary>
+        /// The turret is not doing anything.
+        /// </summary>
+        Idle,
+
+        /// <summary>
+        /// The turret is currently charging up a shot.
+        /// </summary>
+        ChargingUp,
+
+        /// <summary>
+        /// The turret is currently firing a shot.
+        /// </summary>
+        Firing
+    }
+
+    /// <summary>
     /// The furthest distance that the turret can detect enemies.
     /// </summary>
     [Header("Turret Properties")]
@@ -17,16 +38,42 @@ public class RailgunTurret : MonoBehaviour
     private Transform target;
 
     /// <summary>
-    /// The number of times the turret will attempt to charge itself up per second.
-    /// Note that if a charge is in progress, it will ignore consecutive attempts to start
-    /// charging.
+    /// The duration the turret will take before it begins charging up.
     /// </summary>
-    public float firingRate;
+    [Tooltip("The duration the turret will wait before it begins charging up.")]
+    public float waitTimeBeforeCharging;
 
     /// <summary>
-    /// The duration before the next charging up attempt.
+    /// A countdown timer that will charge the turret up when it ends.
+    /// </summary>
+    private float waitTimeCountdown;
+
+    /// <summary>
+    /// How long the turret will take, in seconds, to charge up a shot.
+    /// </summary>
+    [Tooltip("How long the turret will take when it charges up a shot.")]
+    public float chargingDuration;
+
+    /// <summary>
+    /// A countdown that lets the turret finish charging at the end.
+    /// </summary>
+    private float chargingCountdown;
+
+    /// <summary>
+    /// How long the turret will fire its shot before stopping.
+    /// </summary>
+    [Tooltip("How long the turret will fire its shot before stopping.")]
+    public float firingDuration;
+
+    /// <summary>
+    /// A countdown where, at the end, the turret will fire.
     /// </summary>
     private float firingCountdown;
+
+    /// <summary>
+    /// The current state of this turret.
+    /// </summary>
+    private TurretState currentTurretState = TurretState.Idle;
 
     /// <summary>
     /// The Animator component responsible for animating this turret.
@@ -42,34 +89,132 @@ public class RailgunTurret : MonoBehaviour
     [Header("Code Properties")]
     public float enemyDetectionPeriod = 0.5f;
 
+    /// <summary>
+    /// Whether the coroutine <see cref="DetectEnemies(float)"/> is running.
+    /// </summary>
+    private bool enemyDetectionRunning = false;
+
+    /// <summary>
+    /// Awake is called when the script instance is being loaded.
+    /// </summary>
+    void Awake()
+    {
+        if (enemyDetectionPeriod <= 0.1f)
+        {
+            //the enemy detection period is too low and hence, the enemy detection coroutine will run frequently
+            //this code here helps adjust the enemy detection period back to the default value if the current
+            //value is too low.
+            //this also may help prevent the Unity Editor from freezing up, though it is unsure if that will ever occur (I, Brandon, have not tried it).
+            Debug.LogError("Enemy detection period is too low! It's been set back to the default value (0.5f).");
+            enemyDetectionPeriod = 0.5f;
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         //run the coroutine that detects for enemies
         StartCoroutine(DetectEnemies(enemyDetectionPeriod));
 
-        //reset countdown
-        firingCountdown = 1 / firingRate;
+        //reset countdowns
+        waitTimeCountdown = 0; //there's no waiting at the start
+        chargingCountdown = chargingDuration;
+        firingCountdown = firingDuration;
+
+        //assign duration values to the animator parameters (adjusts the speed of the animation)
+        //everything here is the reciprocal of the variable because we are adjusting the speed multiplier, instead of the duration
+        turretAnimator.SetFloat("chargingDuration", 1 / chargingDuration);
+        turretAnimator.SetFloat("firingDuration", 1 / firingDuration);
     }
 
     // Update is called once per frame
     void Update()
     {
+
         if (target)
         {
             //aim at the target
             AimAtTarget();
+        }
 
-            //FIXME: Only set the trigger of charging the railgun once after an enemy is deteced. Do not set trigger when charging up.
-            //charge up an attack
-            ChargeUp();
+        Debug.Log(currentTurretState);
 
-            //fire a laser when the animator's state is in the "Fire" state
-            if (IsFiringState("Railgun Firing"))
-            {
-                //TODO: Insert You Jing's line renderer thingy here (once I know how it works)
-                Debug.Log("FIRE!");
-            }
+        switch (currentTurretState)
+        {
+            //the turret is neither charging up, nor firing, it is just there doing nothing, looking at the enemy (if there's one)
+            case TurretState.Idle:
+
+                //start the coroutine, if it isn't running
+                if (!enemyDetectionRunning)
+                {
+                    StartCoroutine(DetectEnemies(enemyDetectionPeriod));
+                }
+
+                //move on to next state if there is a target and we have waited long enough
+                if (target && waitTimeCountdown <= 0)
+                {
+                    //begin charging up!
+                    currentTurretState = TurretState.ChargingUp;
+
+                    //set the trigger of the animator
+                    turretAnimator.SetTrigger("Charge");
+                }
+                else if (waitTimeCountdown > 0)
+                {
+                    //countdown
+                    waitTimeCountdown -= Time.deltaTime;
+                }
+
+                break;
+
+            //the turret is currently charging up a shot
+            case TurretState.ChargingUp:
+
+                //stop the enemy detection coroutine from running so that we can lock on our target
+                if (enemyDetectionRunning)
+                {
+                    StopCoroutine(DetectEnemies(enemyDetectionPeriod));
+                    enemyDetectionRunning = false;
+                }
+
+                //start the charging countdown
+                chargingCountdown -= Time.deltaTime;
+
+                if (chargingCountdown <= 0)
+                {
+                    //reset the charging countdown
+                    chargingCountdown = chargingDuration;
+
+                    //move on to the firing state
+                    currentTurretState = TurretState.Firing;
+                }
+
+                break;
+
+            case TurretState.Firing:
+
+                //start the firing countdown
+                firingCountdown -= Time.deltaTime;
+
+                if (firingCountdown > 0)
+                {
+                    //this is where we fire the enemy I think
+                    //TODO: Insert You Jing's line renderer thingy here
+                    Debug.Log("Firing at enemy");
+                }
+                else
+                {
+                    //stop firing and go back to idle state
+                    currentTurretState = TurretState.Idle;
+
+                    //reset the firing countdown
+                    firingCountdown = firingDuration;
+
+                    //set a cooldown time before it can charge up and fire again
+                    waitTimeCountdown = waitTimeBeforeCharging;
+                }
+
+                break;
         }
     }
 
@@ -93,22 +238,15 @@ public class RailgunTurret : MonoBehaviour
     private IEnumerator DetectEnemies(float interval)
     {
 
+        //this coroutine is running
+        enemyDetectionRunning = true;
+
         //infinte loop
         while (true)
         {
 
             //check for the nearest enemy
             target = GetNearestEnemy();
-
-            //log our target
-            if (target == null)
-            {
-                Debug.Log("Target is null!");
-            }
-            else
-            {
-                Debug.Log("Target: " + target);
-            }
 
             //suspend execution for 0.5 seconds
             yield return new WaitForSeconds(interval);
@@ -184,19 +322,14 @@ public class RailgunTurret : MonoBehaviour
     private void ChargeUp()
     {
         //start counting down
-        firingCountdown -= Time.deltaTime;
+        waitTimeCountdown -= Time.deltaTime;
 
         //when our countdown hits zero or less...
-        if (firingCountdown <= 0)
+        if (waitTimeCountdown <= 0)
         {
 
             //set a trigger on the animator, which charges up an attack
             turretAnimator.SetTrigger("Charge");
         }
-    }
-
-    private bool IsFiringState(string stateName)
-    {
-        return turretAnimator.GetCurrentAnimatorStateInfo(0).IsName(stateName);
     }
 }
